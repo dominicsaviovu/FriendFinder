a63c4591821540cac703e3170334e3c5b912c4e0
Savio <dominicsaviovu@gmail.com>
2019-10-14 10:04:20 -0600

    added the routing and more

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+
+var friends = [
+    {
+      name: "Hannah Song",
+      photo: "https://avatarfiles.alphacoders.com/114/114209.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Aaron Nguyen",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlQ_sKmLWPTYjiBwj_0-AkEJfFGJ1wezsQHHS50PBcKeSSkdLk",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Lili Vu",
+      photo: "https://uaquidditch.weebly.com/uploads/8/9/3/4/89346246/wo_orig.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Charlie Conway",
+      photo: "https://img.cinemablend.com/cb/d/b/3/4/a/9/db34a9aad5c7936959f1cb489c7ec96ff586f1b319db4b0da05e411b2f900903.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Greg Goldberg",
+      photo: "https://static.wixstatic.com/media/2f062e_7e83d25c7087466e9753e6891bc1adc7.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Sophia Bush",
+      photo: "https://www.stylist.co.uk/images/app/uploads/2018/06/14120224/sophia-bush-sexual-harassment-one-tree-hill-mark-schwahn-crop-1528974150-867x867.jpg?w=256&h=256&fit=max&auto=format%2Ccompress",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+
+  module.exports = friends;
+  
